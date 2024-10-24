fa254edcca1a5510e8262ae129850f2e560a8100
Chetan Soni <chetansoni8101@gmail.com>
2024-10-25 00:20:13 +0530

    home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,7 +6,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Column(
+              children: [
+                const Text(
+                  "This month spending",
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  "₹ 1,00,000",
+                  style: const TextStyle(
+                      fontSize: 36, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+              const SizedBox(height: 16,),
+              Expanded( child: ListView.builder(itemBuilder: (context, index) => Card(child: ListTile(title: Text("₹ 1,000"),),), itemCount: 10, ))
+          ],
+        ),
+      ),
     );
   }
 }
